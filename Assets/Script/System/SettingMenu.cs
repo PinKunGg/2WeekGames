fd5bdb894075abb704f53bf272f203c8079c5ff7
PinKunGg <70133896+PinKunGg@users.noreply.github.com>
2022-07-01 17:16:30 +0700

    **

--- Assets/Script/System/SettingMenu.cs
+++ Assets/Script/System/SettingMenu.cs
@@ -7,11 +7,19 @@ public class SettingMenu : MonoBehaviour
 {
     public GameObject SettingMenu_ui;
 
+    private void Awake() {
+        Application.targetFrameRate = 60;
+    }
+
     private void Start() {
         OnClick_ToggleSettingMenu(false);
     }
 
     private void Update() {
+        if(Application.targetFrameRate != 60){
+            Application.targetFrameRate = 60;
+        }
+
         if(SceneManager.GetActiveScene().buildIndex == 0){return;}
 
         if(Input.GetKeyDown(KeyCode.Escape)){
